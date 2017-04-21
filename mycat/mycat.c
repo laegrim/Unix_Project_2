6d59dd624099665e9de56f840fb18c6bcb33dcf2
Samuel Teich <steich@gmail.com>
2017-04-21 14:16:20 -0500

    started on mycat

---
+++ mycat/mycat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+static void read_input_to_output(char * filepath, bool raw){
+
+}
+
+int main(int argc, char ** argv){
+	
+	//get any options passed by command line
+	while ((int opt = getopt(argc, argv, "")) != -1) {
+		
+		//detect which options, use flags
+		switch (opt) {
+			case :
+
+			default: 
+		}
+
+	}
+	//get the remaining non option arguments
+	
+	//determine where the input is coming from
+	//stdout vs file
+	
+	//exit gracefully
+
+}	
