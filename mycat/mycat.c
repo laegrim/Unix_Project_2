da767246dd1d6f8d7f68eab4caaa908b5a94d026
Samuel Teich <steich@gmail.com>
2017-04-21 23:11:14 -0500

    Completed mycat and makefile for mycat

--- mycat/mycat.c
+++ mycat/mycat.c
@@ -1,28 +1,88 @@
+#include <sys/param.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <err.h>
 
-static void read_input_to_output(char * filepath, bool raw){
+static int read_input_to_output(int input_fd, char * filename){
 
+
+	//get stdout
+	int output_fd = fileno(stdout);
+	
+	//setup a buffer
+	static char * buffer = NULL;
+
+	//do some initialization before reading
+	struct stat fd_info;
+	ssize_t bytes_read, bytes_written = 0;
+	int offset; 
+
+	//get information about fd
+	if (fstat(output_fd, &fd_info)) {//err
+		err(1, "%s", filename);
+	}
+	//set the blocksize
+	size_t blocksize = MAX(fd_info.st_blksize, 1024);
+	//allocate the buffer to be the blocksize
+	if ((buffer = (char *) malloc(blocksize)) == NULL) {//err
+		err(1, "buffer");
+	}
+	
+	//start the read write loop
+	//while there's data to read from the input file, read it
+	while ((bytes_read = read(input_fd, buffer, blocksize)) > 0){
+		//keep track of the difference between the ammount of data read
+		//and the ammount of data written
+		offset = 0;
+		while (bytes_read > bytes_written) {
+			if ((bytes_written = write(output_fd, buffer + offset, (size_t) bytes_read)) < 0) {//err
+				err(1, "stdout");
+			}
+			bytes_read -= bytes_written;
+			offset += bytes_written;
+		}
+	}
+
+	if (bytes_read < 0) {//erri
+		warn("%s", filename);
+		return 1;
+	}		
 }
 
 int main(int argc, char ** argv){
 	
-	//get any options passed by command line
-	while ((int opt = getopt(argc, argv, "")) != -1) {
-		
-		//detect which options, use flags
-		switch (opt) {
-			case :
-
-			default: 
-		}
-
-	}
-	//get the remaining non option arguments
-	
 	//determine where the input is coming from
 	//stdout vs file
+	//there might also be multiple inputs
 	
+	int input_fd;
+	int status = 0;
+	
+	//if argc is 1, then there's been no file provided, input must be from stdin
+	if (argc == 1) {
+		input_fd = STDIN_FILENO;
+		status = read_input_to_output(input_fd, "stdin");
+	}
+	else {
+		for (int i = 1; i < argc; i++) {
+			if((input_fd = open(argv[i], O_RDONLY)) < 0) {//err
+				warn("%s", argv[i]);
+				status = 1;
+			}
+			else {
+				status = read_input_to_output(input_fd, argv[i]);
+				close(input_fd);
+			}
+		}
+	}
+	//attempt to close output stream
+	if (fclose(stdout)) {//err
+		err(1, "stdout");
+	}
 	//exit gracefully
-
+	exit(status);
 }	
--- mysh.h
+++ mysh.h
@@ -9,45 +9,100 @@
 #include <sys/types.h>
 
 
-void check_for_redirect_in(char ** line_tokens){
+void check_for_redirect_in(char ** line_tokens, int token_count){
 	//check for and handle the redirection token <
-
+	
+	//loop through and look at each token
+	for (int i = 0; i < token_count; i++) {
+		//if the token is a stdin redirection token
+		if(line_tokens[i] == '<') {
+			//the immediate right hand side token is the source of the redirect
+			//get a file descriptor for that
+			if (i == token_count - 1) {
+				//we have an error, next token is null terminator
+			}
+			char * filepath = line_tokens[curr_tok_idx + 1];
+			if (int source_fd = open(filepath, O_WRONLY|O_CREAT|O_TRUNC, 0600)) {
+				//we have an error
+			}
+			//duplicate the file descriptor so that it is at stdin
+			if (dup2(source_fd, 0) < 0){
+				//we have an error
+			}
+			//close the old file descriptor
+			close(source_fd);
+			//remove the redirection token and redirection source from the line
+			char ** tmp = (char **) malloc(sizeof(char *) * (token_count - 2));
+			for (int j = 0; j < i; j++) {tmp[j] = line_tokens[j];}
+			for (int j = i + 2; j < token_count; j++) {tmp[j - 2] = line_tokens[j];}
+		        line_tokens = tmp;
+			break;	
+		}
+	}
 }
 
-void check_for_redirect_out(char ** line_tokens){
+void check_for_redirect_out(char ** line_tokens, int token_count){
 	//check for and handle the redirection token >
+	
+	//loop through and look at each token
+	for (int i = 0; i < token_count; i++) {
+		//if the token is a stdin redirection token
+		if(line_tokens[i] == '>') {
+			//the immediate right hand side token is the source of the redirect
+			//get a file descriptor for that
+			if (i == token_count - 1) {
+				//we have an error, next token is null terminator
+			}
+			char * filepath = line_tokens[curr_tok_idx + 1];
+			if (int source_fd = open(filepath, O_WRONLY|O_CREAT|O_TRUNC, 0600)) {
+				//we have an error
+			}
+			//duplicate the file descriptor so that it is at stdin
+			if (dup2(source_fd, 1) < 0){
+				//we have an error
+			}
+			//close the old file descriptor
+			close(source_fd);
+			//remove the redirection token and redirection source from the line
+			char ** tmp = (char **) malloc(sizeof(char *) * (token_count - 2));
+			for (int j = 0; j < i; j++) {tmp[j] = line_tokens[j];}
+			for (int j = i + 2; j < token_count; j++) {tmp[j - 2] = line_tokens[j];}
+		        line_tokens = tmp;
+			break;	
+		}
+	}
+
 }
 
-bool check_for_pipe(char ** line_tokens, char *** pipe_segments, int * p){
+bool check_for_pipes(char ** line_tokens, int token_count, char *** pipe_segments, &num_segments){
 	//Check for and handle the pipe token |
-
 }
 
-void check_for_errors(char ** line_tokens){
+void check_for_errors(char ** line_tokens, int token_count){
 	//Check the line for any obvious errors
 }
 
-void handle_error(int &status){
-}
-
-
 //This function handles the execution
-void exec_line(char ** line_tokens){
+void exec_line(char ** line_tokens, int token_count){
 	
 	pid_t pid;
 	int status;
 	char *** pipe_segments;
-	bool is_pipe;
-	int p[2];
-
-	//check tokens for a pipe
-	is_pipe = check_for_pipe(line_tokens, pipe_segments, p);
+	int num_segments;
 	
 	//if there's a pipe, then we need to execute pipe segements and redirect 
 	//the output from one segment to the input of the next
-	if (is_pipe){
+	if (check_for_pipes(line_tokens, token_count, pipe_segments, num_segments)){
 		
 		int segment = 0;
+
+		//create all the pipes needed up front
+		int ** pipes = malloc(sizeof(int) * 2 * (num_segments));
+		for (int i = 0; i < num_segments; i ++) {
+			pipes[i] = int[2];
+			pipe(pipes[i]);
+		}
+
 
 		//while there are still pipe segments (must be at least 2)
 		while (pipe_segments[segment] != NULL) {
@@ -58,6 +113,7 @@ void exec_line(char ** line_tokens){
 			//if you are the child
 			if (pid == 0) {
 				
+				int * p = pipes[segment]	
 				//if you aren't the last, push stdout throught the pipe
 				if (pipe_segments[segment + 1] != NULL) {dup2(p[1], 1);}
 				//if you aren't the first, pull stdin from the pipe
@@ -68,11 +124,13 @@ void exec_line(char ** line_tokens){
 
 				//get your pipe segment
 				command = pipe_segments[segment];
+				command_tokens = 0;
+				do {command_tokens++;} while (command[command_tokens] != NULL);
 				//check for any errors in the command
-				check_for_errors(command);
+				check_for_errors(command, command_tokens);
 				//check for any redirection (takes precedence over the pipe)
-				check_for_redirect_in(command);
-				check_for_redirect_out(command);
+				check_for_redirect_in(command, command_tokens);
+				check_for_redirect_out(command, command_tokens);
 				//execute the command
 				execvp(command[0], command[1]);
 			}
@@ -82,12 +140,13 @@ void exec_line(char ** line_tokens){
 			}
 			//handle any errors from the forking
 			else {
-				handle_error(pid);
 			}
 		}
-		//close the pipe
-		close(p[0]);
-		close(p[1]);	
+		//close all of the pipes
+		for (int i = 0; i < num_segments; i++){
+			close(pipes[i][0]);
+			close(pipes[i][1]);
+		}
 	}
 	else {
 		//fork a child process
@@ -95,21 +154,20 @@ void exec_line(char ** line_tokens){
 		//if we are the child, then
 		if (pid == 0){ 
 			//do some error checking on the tokens
-			check_for_errors(line_tokens);
+			check_for_errors(line_tokens, token_count);
 			//check for redirection token (<)
-			check_for_reidrect_in(line_tokens);
+			check_for_reidrect_in(line_tokens, token_count);
 			//check for redirection token (>)
-			check_for_redirect_out(line_tokens);
+			check_for_redirect_out(line_tokens, token_count);
 			//execute the commands
 			execvp(line_tokens[0], line_tokens[1]);
 		}
 		// if we are the parent, then wait for the child to finish
 		else if (pid > 0){
-			pid = waitpid(pid, &status, 0)
+			pid = waitpid(pid, &status, 0);
 		}
 		//if the pid is < 0, then there was an error forking; handle it
 		else {
-			handle_error(pid)
 		}
 	}
 }
